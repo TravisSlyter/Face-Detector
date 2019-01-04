57dd1fc9986cadc669636198c007d602af5771a4
TravisSlyter <42150524+TravisSlyter@users.noreply.github.com>
2019-01-04 12:26:48 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 200
+      }
+    }
+  }
+}
+
+  
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className='particles'
+              params={particlesOptions}
+            />
+       <Navigation />
+       <Logo />
+       <Rank />
+       <ImageLinkForm />
+       {/*<FaceRecognition />*/}
       </div>
     );
   }
